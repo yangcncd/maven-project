c4c7581ef870329c37e119637c8a7bcc900e6ed4
Yang Ding <yang.ding@prime-force.com>
2019-08-19 12:58:15 +0200

    Test unused method

--- server/src/main/java/com/example/Person.java
+++ server/src/main/java/com/example/Person.java
@@ -19,4 +19,8 @@ public class Person {
   public final String person(final String someone)     {
     return String.format("Hello, %s!", someone);
   }
+  
+  private String getTeststr(){
+	  return "";
+  }
 }
